3dc6509ee61fd5fd08904c4da977d5592b9463c1
azimut <azimut.github@protonmail.com>
2020-07-28 18:37:24 -0300

    global channels

--- src/patterns.lisp
+++ src/patterns.lisp
@@ -1,13 +1,16 @@
 (in-package #:launchpad-csound)
-
-;; Scenes sharing a long pattern (?
-;; TODO: special light color? root and intervals?
-;; TODO: multiple touches/colors for different effects
-;; TODO: pick random in row
-;; TODO: random key
-
+;; TODO: per stage: velocity legato program bank
+;; TODO: global: mode root
+;; TODO: Offset keys (will have to move all current keys)
+;;       Might be don't delete keys outside grid
+;; TODO: Special light color?
+;;       Multiple touches/colors for different effects
+;; TODO: Arpeggiator OR repeat key Nth times in timeslot
+;;       Pick random in row
+;;       Play 1 each Nth of cycles
+;;       Random key prob of play, mutuallyy exclusive in row?
+;;       Transpose each nth cycles
 (defvar *keys* nil)
-(defvar *muted-beats* ())
 
 (defclass patterns (main)
   ((index :initform 0
@@ -18,24 +21,53 @@
           :documentation "Beat durations for each SCENE"))
   (:default-initargs :layout :xy))
 
+(defun relight-different (old new)
+  (let ((muted (chan-muted (aref *chans* old)))
+        (playing (has-keys-p old)))
+    (when (and (not playing) (not muted))
+      (launchpad:button-scene-xy-off old))
+    (when (and playing (not muted))
+      (launchpad:button-scene-xy-on old (launchpad:color :lg)))
+    (when muted
+      (launchpad:button-scene-xy-on old (launchpad:color :lr))))
+  (let ((muted (chan-muted (aref *chans* new))))
+    (if muted
+        (launchpad:button-scene-xy-on new (launchpad:color :lr))
+        (launchpad:button-scene-xy-on new (launchpad:color :lo)))))
+
+(defun relight-equal (scene)
+  (let ((muted (chan-muted (aref *chans* scene))))
+    (if muted
+        (launchpad:button-scene-xy-on scene (launchpad:color :lo))
+        (launchpad:button-scene-xy-on scene (launchpad:color :lr)))
+    (setf (chan-muted (aref *chans* scene))
+          (not (chan-muted (aref *chans* scene))))))
+
+(defun relight-page (scene)
+  (let ((muted (chan-muted (aref *chans* scene)))
+        (playing (has-keys-p scene)))
+    (when muted
+      (launchpad:button-scene-xy-on scene (launchpad:color :lr)))
+    (when (and playing (not muted))
+      (launchpad:button-scene-xy-on scene (launchpad:color :lg)))))
+
 (defmethod (setf index) :around (new-value (server patterns))
-  (if (= new-value (slot-value server 'index))
-      (if (position new-value *muted-beats*)
-          (alexandria:deletef *muted-beats* new-value :test #'=)
-          (pushnew new-value *muted-beats* :test #'=))
-      (call-next-method)))
+  (if (= new-value (slot-value server 'index)); un/mute if same
+      (progn (relight-equal new-value))
+      (progn (relight-different (slot-value server 'index) new-value)
+             (call-next-method))))
 (defmethod (setf index) :before (new-value (server patterns))
   (check-type new-value (integer 0 7))
-  (relight-scene (slot-value server 'index) 0)
-  (relight-pages (slot-value server 'index)))
+  (relight-scene (slot-value server 'index) 0))
 (defmethod (setf index) :after (new-value (server patterns))
-  (relight-scene new-value (launchpad:color :hg)))
+  (relight-scene new-value (launchpad:color :lg)))
 
 (defmethod change-class :after (obj (new (eql 'patterns)) &rest initargs)
   (declare (ignore initargs))
   (launchpad:reset)
   (cl-rtmidi:with-midi-oss-out (cl-rtmidi:*default-midi-out-stream* "/dev/midi1")
-    (relight-scene 0 (launchpad:color :hg)))
+    (relight-scene 0 (launchpad:color :lg)))
+  (mapcar #'relight-page (alexandria:iota 8))
   (setf (layout obj) :xy))
 
 (defmethod launchpad:connect :after ((server patterns))
@@ -79,41 +111,42 @@
                                 to-color))))
    *keys*))
 
-(defun relight-pages (old-scene)
-  (if (has-keys-p old-scene)
-      (launchpad:button-scene-xy-on  old-scene (launchpad:color :lg))
-      (launchpad:button-scene-xy-off old-scene)))
-
-(defun idur (scene)
+(defun ivel (scene)
   (case scene
-    ((0 1) 1.7)
-    ((2 3)  .7)
-    (t .3)))
+    ((1) 60)
+    (t 80)))
+(defun idur (scene dur)
+  (case scene
+    ((1) 2)
+    (t (* dur 1.7)))
+  #+nil
+  (case scene
+    ((0 1) 1.8)
+    ((2 3) 0.8)
+    ((4 5) 0.4)
+    (t 0.1)))
 
-(let* (;;(scale (ego::scale 4 :phrygian))
-       ;;(cscale (cm:new cm:cycle :of scale))
-       )
-  (defun step-keys (stepping-scene stepping-column scale)
-    (serapeum:do-hash-table (k v *keys*)
-      (destructuring-bind (scene midi) k
-        (destructuring-bind (row col) v
-          (when (and (= scene stepping-scene)
-                     (= col stepping-column))
-            (cloud:schedule
-             *csound*
-             1;;(iname (+ (mod scene 4) 1) midi)
-             0
-             (idur scene) ;;(+ (idur scene) (ego::cosr .2 .1 (+ scene)))
-             #+nil
-             (if (or (= scene 4))
-                 (+ 60 (car (cm:next cscale (+ 1 row)))))
-             (ego::pc-relative (+ 48 (* 24 (mod scene 2)))
-                               row
-                               scale)
-             ;;(ego::rcosr 60 10 (+ 1 scene))
-             40
-             )))))))
-
+(defun step-keys (stepping-scene stepping-column scale dur)
+  (serapeum:do-hash-table (k v *keys*)
+    (destructuring-bind (scene midi) k
+      (destructuring-bind (row col) v
+        (when (and (= scene stepping-scene)
+                   (= col stepping-column))
+          (cloud:schedule
+           *csound*
+           (iname (1+ scene) midi)
+           0
+           (idur scene dur);;(+ (idur scene) (ego::cosr .2 .1 (+ scene)))
+           #+nil
+           (if (or (= scene 4))
+               (+ 60 (car (cm:next cscale (+ 1 row)))))
+           (ego::pc-relative (+ 48 (* 24 (mod scene 2)))
+                             row
+                             scale)
+           ;;(ego::rcosr 15 5 (+ 1 scene))
+           (ivel scene)
+           ))))))
+;;'(.1 .2 .3 .5 .8 1 1.3 1.5 1.7 2 3 4)
 (defun light-beat (time duration column)
   (eat time #'launchpad:button-automap-on column (launchpad:color :lg))
   (eat (+ duration time) #'launchpad:button-automap-off column))
@@ -124,9 +157,10 @@
     (when (and (slot-exists-p *csound* 'index); work on change-class
                (= idx (index *csound*)))
       (light-beat time dur column))
-    (when (not (position idx *muted-beats* :test #'=))
+    (when (not (chan-muted (aref *chans* idx)))
       (eat time #'step-keys idx column
-           (ego::scale (root *csound*) (mode *csound*))))
+           (ego::scale (root *csound*) (mode *csound*))
+           dur))
     (eat next-time #'beat
          next-time idx dur (a:rotate (copy-seq cycle) -1))))
 
@@ -134,19 +168,15 @@
   (member key '(8 24 40 56 72 88 104 120) :test #'=))
 
 (defmethod launchpad:handle-input :after ((server patterns) raw-midi)
-  (let ((chan 1;;(mod (index server) 4)
-              )
-        (scene (index server)))
+  (let* ((scene (index server))
+         (chan  (1+ scene)))
     (trivia:match raw-midi
       ((list 176 104 127) (prev-program server chan))
       ((list 176 105 127) (next-program server chan))
       ((list 176 111 127) (change-class server (next-class)))
-      ((trivia:guard ; Scene Buttons
-        (list 144 key 127) (is-control key))
-       (progn (launchpad:raw-command #x90 key (launchpad:color :lo))
-              (setf (index server) (launchpad:xy key))))
-      ((trivia:guard ; Grid Buttons
-        (list 144 key 127) (not (is-control key)))
+      ((trivia:guard (list 144 key 127) (is-control key))
+       (setf (index server) (launchpad:xy key)))
+      ((trivia:guard (list 144 key 127) (not (is-control key)))
        (if (key-pressed-p key scene)
            (progn (launchpad:raw-command #x80 key 0)
                   (remove-key scene key))
--- src/presets.lisp
+++ src/presets.lisp
@@ -1,16 +1,20 @@
 (in-package #:launchpad-csound)
 
-(defparameter *preset*
-  (list (a:iota 110)
-        (a:iota 110)
-        (a:iota 110)
-        (a:iota 110)
-        (a:iota 110)
-        (a:iota 110)
-        (a:iota 110)
-        (a:iota 110)
-        (a:iota 110))
-  "Global per channel program presets, needs persist between change-class")
+;; chorus/reverb
+(defstruct chan
+  (preset   (a:iota 110))
+  (bank     (a:iota 2))
+  (offset   (a:iota 10 :start 0 :step .1))
+  (legato   (a:rotate (a:iota 20 :start .1 :step .1) 10))
+  (muted    NIL)
+  (velocity (a:rotate (a:iota 50 :start 20 :step 2) 25)))
+
+(defvar *chans* nil)
+
+(defun init-channels (&optional (nr 9))
+  (unless *chans*
+    (setf *chans* (->> (loop repeat nr collect (make-chan))
+                       (make-array nr :initial-contents)))))
 
 (defun change-program (server channel program)
   (let ((msg (format nil "fluidProgramSelect giengine1, ~d, gisfnum1, 0, ~d"
@@ -22,12 +26,14 @@
   (change-program
    server
    channel
-   (first (setf (elt *preset* (1- channel)) (a:rotate (copy-seq (elt *preset* (1- channel))) -1)))))
+   (first
+    (setf (chan-preset (aref *chans* (1- channel)))
+          (a:rotate (copy-seq (chan-preset (aref *chans* (1- channel)))) +1)))))
 
 (defun next-program (server channel)
   (change-program
    server
    channel
-   (first (setf (elt *preset* (1- channel)) (a:rotate (copy-seq (elt *preset* (1- channel))) +1)))))
-
-
+   (first
+    (setf (chan-preset (aref *chans* (1- channel)))
+          (a:rotate (copy-seq (chan-preset (aref *chans* (1- channel)))) -1)))))
